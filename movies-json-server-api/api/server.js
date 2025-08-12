09e95e8a8bacaac3c4f62f632a8a020fed45bd53
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2025-08-12 19:29:03 -0300

    fix on server.js

--- movies-json-server-api/api/server.js
+++ movies-json-server-api/api/server.js
@@ -19,7 +19,7 @@ const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// server.use(router);
+server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server is running on port 3000');
 });
