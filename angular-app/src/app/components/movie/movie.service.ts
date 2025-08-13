8e6abf96da129d51cb47765f6e287cdd6be426f7
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2025-08-13 03:05:38 -0300

    update apiUrl environment variable

--- angular-app/src/app/components/movie/movie.service.ts
+++ angular-app/src/app/components/movie/movie.service.ts
@@ -3,8 +3,10 @@ import { Movie } from './movie.model';
 import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
-import { API } from 'src/app/app.api';
+// import { API } from 'src/app/app.api';
 import { SharedService } from '../shared/shared.service';
+import { environment } from '../../../environments/environment';
+const apiUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -26,35 +28,35 @@ export class MovieService {
   }
 
   create(movie: Movie): Observable<Movie> {
-    return this.http.post<Movie>(`${API}/movies`, movie).pipe(
+    return this.http.post<Movie>(`${apiUrl}/movies`, movie).pipe(
       map(obj => obj), 
       catchError(e => this.handleError(e))
     );
   }
 
   retrieve(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${API}/movies`).pipe(
+    return this.http.get<Movie[]>(`${apiUrl}/movies`).pipe(
       map(obj => obj), 
       catchError(e => this.handleError(e))
     );
   }
 
   getById(id: string): Observable<Movie> {
-    return this.http.get<Movie>(`${API}/movies/${id}`).pipe(
+    return this.http.get<Movie>(`${apiUrl}/movies/${id}`).pipe(
       map(obj => obj), 
       catchError(e => this.handleError(e))
     );
   }
 
   update(movie: Movie): Observable<Movie> {
-    return this.http.put<Movie>(`${API}/movies/${movie.id}`, movie).pipe(
+    return this.http.put<Movie>(`${apiUrl}/movies/${movie.id}`, movie).pipe(
       map(obj => obj), 
       catchError(e => this.handleError(e))
     );
   }
 
   delete(id: number): Observable<Movie> {
-    return this.http.delete<Movie>(`${API}/movies/${id}`).pipe(
+    return this.http.delete<Movie>(`${apiUrl}/movies/${id}`).pipe(
       map(obj => obj), 
       catchError(e => this.handleError(e))
     );
